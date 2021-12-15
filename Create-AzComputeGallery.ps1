b0521d15a738aba9db8bbaeff1e6c95632524dcb
Louis Zhang (PACTERA TECHNOLOGIES INC) <v-yizha4@microsoft.com>
2021-12-14 18:29:51 -0800

    initial commit

---
+++ Create-AzComputeGallery.ps1
@@ -0,0 +1,85 @@
+#Source of script, https://docs.microsoft.com/en-us/azure/virtual-machines/windows/image-builder-powershell#next-steps
+
+#Create an Azure Compute Gallery
+$myGalleryName = '<gallery name>'
+$imageDefName = '<image def name>'
+
+New-AzGallery -GalleryName $myGalleryName -ResourceGroupName $imageResourceGroup -Location $location
+
+#Create a gallery definition.
+$GalleryParams = @{
+  GalleryName = $myGalleryName
+  ResourceGroupName = $imageResourceGroup
+  Location = $location
+  Name = $imageDefName
+  OsState = 'generalized'
+  OsType = 'Windows'
+  Publisher = 'myCo'
+  Offer = 'Windows'
+  Sku = 'Win2019'
+}
+New-AzGalleryImageDefinition @GalleryParams
+
+#Create an Azure image builder source object. 
+#See Find Windows VM images in the Azure Marketplace with Azure PowerShell for valid parameter values.
+$SrcObjParams = @{
+  SourceTypePlatformImage = $true
+  Publisher = 'MicrosoftWindowsServer'
+  Offer = 'WindowsServer'
+  Sku = '2019-Datacenter'
+  Version = 'latest'
+}
+$srcPlatform = New-AzImageBuilderSourceObject @SrcObjParams
+
+#Create an Azure image builder distributor object.
+$disObjParams = @{
+  SharedImageDistributor = $true
+  ArtifactTag = @{tag='dis-share'}
+  GalleryImageId = "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup/providers/Microsoft.Compute/galleries/$myGalleryName/images/$imageDefName"
+  ReplicationRegion = $location
+  RunOutputName = $runOutputName
+  ExcludeFromLatest = $false
+}
+$disSharedImg = New-AzImageBuilderDistributorObject @disObjParams
+
+#Create an Azure image builder customization objects.
+#1st customization
+$ImgCustomParams01 = @{
+  PowerShellCustomizer = $true
+  CustomizerName = 'settingUpMgmtAgtPath'
+  RunElevated = $false
+  Inline = @("mkdir c:\\buildActions", "mkdir c:\\buildArtifacts", "echo Azure-Image-Builder-Was-Here  > c:\\buildActions\\buildActionsOutput.txt")
+}
+$Customizer01 = New-AzImageBuilderCustomizerObject @ImgCustomParams01
+#2nd customization
+$ImgCustomParams02 = @{
+  FileCustomizer = $true
+  CustomizerName = 'downloadBuildArtifacts'
+  Destination = 'c:\\buildArtifacts\\index.html'
+  SourceUri = 'https://raw.githubusercontent.com/azure/azvmimagebuilder/master/quickquickstarts/exampleArtifacts/buildArtifacts/index.html'
+}
+$Customizer02 = New-AzImageBuilderCustomizerObject @ImgCustomParams02
+
+#Create an Azure image builder template.
+#Compose the source object, distributor, customizations.
+$ImgTemplateParams = @{
+  ImageTemplateName = $imageTemplateName
+  ResourceGroupName = $imageResourceGroup
+  Source = $srcPlatform
+  Distribute = $disSharedImg
+  Customize = $Customizer01, $Customizer02
+  Location = $location
+  UserAssignedIdentityId = $identityNameResourceId
+}
+New-AzImageBuilderTemplate @ImgTemplateParams
+
+
+#Check Template creating status.
+Get-AzImageBuilderTemplate -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup |
+  Select-Object -Property Name, LastRunStatusRunState, LastRunStatusMessage, ProvisioningState
+
+#Remove template.
+Remove-AzImageBuilderTemplate -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup
+
+#Start the image build, Submit the image configuration to the VM image builder service.
+Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName
